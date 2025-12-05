0a29fa29fa6f713d9b6620f07b0820c13ef14707
ARTIST <triplezgamex@gmail.com>
2025-12-05 19:28:53 +0530

    Add files via upload

--- styles/index.js
+++ styles/index.js
@@ -5,12 +5,12 @@ function showProduct(name,price,img,desc){
   document.getElementById("popup-img").src = img;
   document.getElementById("popup-desc").innerHTML = desc;
 
-  // AUTO WHATSAPP REDIRECT LINK (CHANGE NUMBER)
-  let phone = "91XXXXXXXXXX"; // ← YOUR NUMBER HERE (NO + SIGN)
+  // 🔥 Instagram DM redirect instead of WhatsApp
+  let username = "daydream_beads_";  // <<< Your Insta Username
   let message = `Hello, I want to order ${name} for ${price}`;
-
-  document.getElementById("whatsapp-link").href =
-  "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+  
+  document.getElementById("insta-link").href =
+  `https://www.instagram.com/direct/t/${username}?text=${encodeURIComponent(message)}`;
 
   document.getElementById("popup-box").style.display="block";
   document.getElementById("overlay").style.display="block";
@@ -20,4 +20,4 @@ function closePopup(){
   document.getElementById("popup-box").style.display="none";
   document.getElementById("overlay").style.display="none";
 }
-
+document.getElementById("overlay").onclick = closePopup;
